17e3ed0e348cd2804985f7d0449f2c87b098ffbc
petudeveloper <davidalvarezmazzo@gmail.com>
2021-12-03 12:13:20 -0500

    Add missing Router tag that wraps the app component.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import store from './redux/ConfigureStore';
+import { fetchData } from './redux/reducer';
+
+store.dispatch(fetchData());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
